a7d7f16b7908129759821dd82c8775812e58919d
Madison Kendall <lilyzkendall@gmail.com>
2022-03-09 11:53:33 -0700

    added the rest of files/folders and current date/time

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Hi! Welcome to my increase-productivity-planner code!  
+
+// appends the date today to the webpage
+const today = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").append(today);
+console.log(today.format);
+console.log("hello world");
+
+
+
+
+
+
+
+
+
+
+
+// Madison Kendall increase-productivity-planner code 
