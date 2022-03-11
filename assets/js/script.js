2fd2e77d056b70771db6cb27fc299765c5e4844f
Madison Kendall <lilyzkendall@gmail.com>
2022-03-11 13:20:57 -0700

    used for loop to add elements to the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,40 @@
 
 // appends the date today to the webpage
 const today = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").append(today);
-console.log(today.format);
-console.log("hello world");
+$("#currentDay").html(today);
 
-const 
+let schedule = [];
+
+loadPage = function() {
+    for(let i=9; i<18; i++) {
+       schedule.push({"hour": i});
+    }
+    for(let i=0; i < schedule.length; i++) {
+      console.log(schedule[i]);
+
+      let hourDiv = $("<div>").addClass("row time-block");
+      const timeVal = schedule[i].hour;
+      let timeString = "";
+      if(timeVal < 12) {
+        timeString = timeVal + " AM";
+      } else if(timeVal > 12) {
+        const adjustedTime = timeVal - 12;
+        timeString = adjustedTime + " PM";
+      } else {
+        timeString = "12 PM";
+      }
+
+      let time = $("<textarea>").text(timeString).addClass("description");
+      let input = $("<input>").attr("placeholder", "Enter note here").addClass("toDo-input time-block");
+      saveBtn = $("<button>").addClass("fas fa-save saveBtn")
+      $(hourDiv).append(time).append(input).append(saveBtn);
+      $(".planner").append(hourDiv);
+    }
+  };
 
 
 
 
-
-
-
-
+loadPage();
 
 // Madison Kendall increase-productivity-planner code 
