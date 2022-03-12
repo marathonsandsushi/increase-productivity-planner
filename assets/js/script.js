4fff98bfc7855257a17cc93eb664e8c7cb0ea6c1
Madison Kendall <lilyzkendall@gmail.com>
2022-03-12 13:55:26 -0700

    organized loadPage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,49 +7,26 @@ $("#currentDay").html(today);
 let schedule = [];
 
 loadPage = function () {
-    
-    for (let i = 9; i < 18; i++) {
-        schedule.push({ hour: i });
-    }
+  for (let i = 9; i < 18; i++) {
+    schedule.push({ hour: i });
+  }
 
-    for (let i = 0; i < schedule.length; i++) {
+  for (let i = 0; i < schedule.length; i++) {
+    console.log(schedule[i]);
 
-        console.log(schedule[i]);
+    // creates hour div
+    let hourDiv = $("<div>").addClass("row time-block");
+    let timeString = createTimeString(i);
+    let input = createInputId(i);
+    let saveBtn = createBtnId(i);
 
-        // creates hour div 
-        let hourDiv = $("<div>").addClass("row time-block");
-        const timeVal = schedule[i].hour;
-        let timeString = "";
-        if (timeVal < 12) {
-          timeString = timeVal + " AM";
-        } else if (timeVal > 12) {
-          const adjustedTime = timeVal - 12;
-          timeString = adjustedTime + " PM";
-        } else {
-          timeString = "12 PM";
-        }
+    let hourDisplay = $("<div>").text(timeString).addClass("col-md-1 hour");
 
-        let hourDisplay = $("<div>").text(timeString).addClass("col-md-1 hour");
-
-        const inputId = "input-" + i;
-        let input = $("<input>")
-          .attr("placeholder", "Enter note here")
-          .addClass("col-lg time-block description")
-          .attr("id", inputId);
-        const btnId = "btn-" + i;
-        saveBtn = $("<button>")
-          .addClass("fas fa-save saveBtn")
-          .attr("id", btnId)
-          .click(function () {
-            saveEvent(i);
-          });
-
-        $(hourDiv).append(hourDisplay).append(input).append(saveBtn);
-        $(".planner").append(hourDiv);
-    }
+    $(hourDiv).append(hourDisplay).append(input).append(saveBtn);
+    $(".planner").append(hourDiv);
+  }
 };
 
-// save 
 saveEvent = function (i) {
   console.log(i);
   const inputId = "input-" + i;
@@ -57,8 +34,43 @@ saveEvent = function (i) {
   const eventText = eventInput.value;
   console.log(eventInput);
   console.log(eventText);
-  schedule[i].event =eventText;
+  schedule[i].event = eventText;
   localStorage.schedule = JSON.stringify(schedule);
+};
+
+createInputId = function(i) {
+    const inputId = "input-" + i;
+    let input = $("<input>")
+      .attr("placeholder", "Enter note here")
+      .addClass("col-lg time-block description")
+      .attr("id", inputId);
+      return input;
+};
+
+createBtnId = function(i) {
+  const btnId = "btn-" + i;
+  saveBtn = $("<button>")
+    .addClass("fas fa-save saveBtn")
+    .attr("id", btnId)
+    .click(function () {
+      saveEvent(i);  
+    }
+    );
+    return saveBtn;
+  }
+
+createTimeString = function (i) {
+  const timeVal = schedule[i].hour;
+  let timeString = "";
+  if (timeVal < 12) {
+    timeString = timeVal + " AM";
+  } else if (timeVal > 12) {
+    const adjustedTime = timeVal - 12;
+    timeString = adjustedTime + " PM";
+  } else {
+    timeString = "12 PM";
+  }
+  return timeString;
 };
 
 loadPage();
