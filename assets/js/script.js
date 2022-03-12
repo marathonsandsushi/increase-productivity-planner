5fd0f596010df5a556a0411b7c89bf83c503ef2b
Madison Kendall <lilyzkendall@gmail.com>
2022-03-12 10:12:40 -0700

    got id of input field

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,67 @@
-// Hi! Welcome to my increase-productivity-planner code!  
+// Hi! Welcome to my increase-productivity-planner code!
 
 // appends the date today to the webpage
-const today = moment().format('MMMM Do YYYY, h:mm:ss a');
+const today = moment().format("MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").html(today);
 
 let schedule = [];
 
-loadPage = function() {
-    for(let i=9; i<18; i++) {
-       schedule.push({"hour": i});
+loadPage = function () {
+  for (let i = 9; i < 18; i++) {
+    schedule.push({ hour: i });
+  }
+  for (let i = 0; i < schedule.length; i++) {
+    console.log(schedule[i]);
+
+    // <!-- Time: 3 PM -->
+    // <div id="hour15" class="row time-block">
+    let hourDiv = $("<div>").addClass("row time-block");
+    const timeVal = schedule[i].hour;
+    let timeString = "";
+    if (timeVal < 12) {
+      timeString = timeVal + " AM";
+    } else if (timeVal > 12) {
+      const adjustedTime = timeVal - 12;
+      timeString = adjustedTime + " PM";
+    } else {
+      timeString = "12 PM";
     }
-    for(let i=0; i < schedule.length; i++) {
-      console.log(schedule[i]);
 
-      // <!-- Time: 3 PM -->
-      // <div id="hour15" class="row time-block">
-      let hourDiv = $("<div>").addClass("row time-block");
-      const timeVal = schedule[i].hour;
-      let timeString = "";
-      if(timeVal < 12) {
-        timeString = timeVal + " AM";
-      } else if(timeVal > 12) {
-        const adjustedTime = timeVal - 12;
-        timeString = adjustedTime + " PM";
-      } else {
-        timeString = "12 PM";
-      }
-      
-      //   <div class="col-md-1 hour">3 PM</div>
-      let hourDisplay = $("<div>").text(timeString).addClass("col-md-1 hour")
-      //   <textarea class="col-md-10 description"></textarea>
-     // let time = $("<textarea>").addClass("description");
+    //   <div class="col-md-1 hour">3 PM</div>
+    let hourDisplay = $("<div>").text(timeString).addClass("col-md-1 hour");
+    //   <textarea class="col-md-10 description"></textarea>
+    // let time = $("<textarea>").addClass("description");
 
+    //   <button class="btn saveBtn cold-md-1"><i class="fas fa-save"></i></button>
+    // </div>
 
-      //   <button class="btn saveBtn cold-md-1"><i class="fas fa-save"></i></button>
-      // </div>
-      
-      
-      
-      let input = $("<input>").attr("placeholder", "Enter note here").addClass("col-lg time-block description");
-      const btnId = "btn-" + i;
-      saveBtn = $("<button>").addClass("fas fa-save saveBtn").attr('id', btnId).click(
-        function() {
-          handleButtonClick(i);
-        }
-      );
+    const inputId = "input-" + i;
+    let input = $("<input>")
+      .attr("placeholder", "Enter note here")
+      .addClass("col-lg time-block description")
+      .attr("id", inputId);
+    const btnId = "btn-" + i;
+    saveBtn = $("<button>")
+      .addClass("fas fa-save saveBtn")
+      .attr("id", btnId)
+      .click(function () {
+        handleButtonClick(i);
+      });
 
-      // saveBtn.setAttribute("onclick", "handleButtonClick("+i+")");
-      // $("button").click(function(){
-      //   $("img").attr("width",function(n, v){
-      //     return v - 50;
-      //   });
-      // });
-      $(hourDiv).append(hourDisplay).append(input).append(saveBtn);
-      $(".planner").append(hourDiv);
-    }
-  };
+    $(hourDiv).append(hourDisplay).append(input).append(saveBtn);
+    $(".planner").append(hourDiv);
+  }
+};
 
- handleButtonClick = function (arrayIndex) {
-console.log(arrayIndex);
- }
-
+handleButtonClick = function (i) {
+  console.log(i);
+  const inputId = "input-" + i;
+  const eventInput = document.getElementById(inputId);
+  const eventText = eventInput.value;
+  console.log(eventInput);
+  console.log(eventText);
+};
 
 loadPage();
 
-// Madison Kendall increase-productivity-planner code 
+// Madison Kendall increase-productivity-planner code
